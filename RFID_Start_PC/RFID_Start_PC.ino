17f59ad8193ed72bcc8ad4817489653ad296080b
ullmannV <ullmann.vojta@gmail.com>
2020-07-10 17:35:23 +0200

    Arduino Project Created

---
+++ RFID_Start_PC/RFID_Start_PC.ino
@@ -0,0 +1,14 @@
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+#include <deprecated.h>
+
+void setup(void) {
+  // put your setup code here, to run once:
+
+}
+
+void loop(void) {
+  // put your main code here, to run repeatedly:
+
+}
