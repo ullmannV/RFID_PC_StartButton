590aba22b0b28004346883cb8b54f73cc0e9d449
ullmannV <ullmann.vojta@gmail.com>
2020-07-10 17:57:37 +0200

    Setting up program's basic structure

--- RFID_Start_PC/RFID_Start_PC.ino
+++ RFID_Start_PC/RFID_Start_PC.ino
@@ -1,14 +1,28 @@
+/* Necessary Libs */
 #include <MFRC522.h>
 #include <MFRC522Extended.h>
 #include <require_cpp11.h>
 #include <deprecated.h>
+/* END of Dependencies */
 
+/* Enumerate of OUTPUT pins name */
+enum { MFRC522_SPI_SS, MFRC522_RST, PWR_BTN };
+/* Array of OUTPUT pins */
+const uint8_t output_pins[] = { 
+  3, /* MFRC522_SPI_SS - SlaveSelect SPI pin for MFRC522 */
+  4, /* MFRC522_RST    - Pin for resetting MFRC522 */
+  5  /* PWR_BTN        - Pin that is connected as substitute of the signal from case's power button */
+  };
+
+/* Init (Executed once) Code */
 void setup(void) {
-  // put your setup code here, to run once:
-
+  /* set all OUTPUT pins as OUTPUT */
+  for (auto pin : output_pins) {
+    pinMode(pin, OUTPUT);
+  }
 }
 
+/* Endlessly running code */
 void loop(void) {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(output_pins[PWR_BTN], HIGH);
 }
